c93349c7a8ca652759f04fae0df21fa6d7eeb6b5
Oshin Gansi <os.gansi@gmail.com>
2022-08-16 10:36:05 +0545

    adds crud api for contacts

---
+++ src/models/ContactModel.ts
@@ -0,0 +1,55 @@
+import db from '../db/db';
+
+import {Contact, ContactToInsert} from '../domain/Contact';
+
+class ContactModel{
+    public static table = "contacts";
+
+    public static async getAllContacts(){
+        const allContacts = await db(ContactModel.table).select(
+            "id","name","contact","email","address","photo"
+        )
+        return allContacts;
+    }
+
+    public static async getContactById(id: number) {
+        const contacts = await db(ContactModel.table).where({ id }).first();
+    
+        return contacts;
+      }
+
+      public static async getContactByName(name: string) {
+        const contact = await db(ContactModel.table).where({ name }).first();
+    
+        return contact;
+      }
+
+      public static async createContact(contact: ContactToInsert) {
+        const newContact = await db(ContactModel.table)
+          .insert(contact)
+          .returning("*")
+    
+        return newContact;
+
+
+      }
+
+      public static async updateContact(contact: Contact) {
+        const updatedContact = await db(ContactModel.table)
+          .where({ id: contact.id })
+          .update(contact)
+          .returning("*");
+    
+        return updatedContact;
+      }
+
+      public static async deleteContact(id: number) {
+        await db(ContactModel.table).where({ id }).delete();
+    
+        return;
+      }
+
+
+}
+
+export default ContactModel;
