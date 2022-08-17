2432b6d83a682dd8a4eccff764ea15ceaf3debe9
Oshin Gansi <os.gansi@gmail.com>
2022-08-17 18:43:54 +0545

    adds gitignore

--- src/routes/contactRoutes.ts
+++ src/routes/contactRoutes.ts
@@ -1,19 +1,14 @@
-import {Router} from "express";
+import { Router } from "express";
 import upload from "../config/multer";
 import * as contactController from "../controllers/contactController";
 
-
 const router = Router();
 
-router.post('/',contactController.getAllContacts);
-router.get('/:id',contactController.getContactById);
-router.get('/:name',contactController.getContactByName);
-router.post('/add',upload.single("photo"),contactController.createContact);
-router.put(
-    "/:id",
-    upload.single("photo"),
-    contactController.updateContact
-  );
-router.delete('/:id',contactController.deleteContact);
+router.post("/", contactController.getAllContacts);
+router.post("/add", upload.single("photo"), contactController.createContact);
+router.get("/:id", contactController.getContactById);
+router.get("/:name", contactController.getContactByName);
+router.put("/:id", upload.single("photo"), contactController.updateContact);
+router.delete("/:id", contactController.deleteContact);
 
 export default router;
